09b3369a4bb3021b64723b03cd14cc70b4d2f7b3
itz-amethyst <pransermi@gmail.com>
2023-06-25 11:37:48 +0430

    Add project files.

---
+++ TowersofHanoi/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TowersofHanoi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TowerOfHanoi T = new TowerOfHanoi();
+
+        string cnumdiscs;
+
+        Console.WriteLine("Enter the number of discs: ");
+
+        cnumdiscs = Console.ReadLine();
+
+        T.numdiscs = Convert.ToInt32(cnumdiscs);
+
+        T.MoveTower(T.numdiscs, 1, 3, 2);
+
+        Console.ReadLine();
+
+    }
+}
---
+++ TowersofHanoi/TowerOfHanoi.cs
@@ -0,0 +1,45 @@
+﻿namespace TowersofHanoi
+{
+    public class TowerOfHanoi
+    {
+         int M_Numdiscs;
+
+         public TowerOfHanoi()
+         {
+             numdiscs = 0;
+         }
+
+         public TowerOfHanoi(int newVal)
+         {
+            numdiscs = newVal;
+         }
+
+         public int numdiscs
+         {
+             get
+             {
+                 return M_Numdiscs;
+             }
+
+             set
+             {
+                 if (value > 0)
+                 {
+                     M_Numdiscs = value;
+                 }
+             }
+         }
+
+         public void MoveTower(int n, int from, int to, int other)
+         {
+             if (n > 0)
+             {
+                 MoveTower(n - 1 , from , other , to);
+
+                 Console.WriteLine($"Move disk {n} From tower {from} to tower {to}" , n , from , to);
+
+                 MoveTower(n-1 , other , to , from);
+             }
+         }
+    }
+}
